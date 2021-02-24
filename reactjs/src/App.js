accdd22e863c91e3895ad1d4a6d3ec6b5b2a9846
Ravindu <ravinduwata2@gmail.com>
2021-02-24 13:49:24 +0530

    Initial commit

---
+++ reactjs/src/App.js
@@ -0,0 +1,41 @@
+//import logo from './logo.svg';
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+
+import {Container, Row, Jumbotron, Col} from 'react-bootstrap';
+import Welcome from "./components/Welcome";
+import Footer from "./components/Footer";
+import Inventory from "./components/Inventory";
+
+function App() {
+
+  return (
+    <Router >
+
+        <NavigationBar />
+        <Switch>
+            <Route path="/" exact component={Welcome} />
+            <Route path="/inventory" exact component={Inventory} />
+        </Switch>
+
+        {/*<Container>
+            <Row>
+                <Col>
+                    <Switch>
+                        <Route path="/" exact component={Welcome} />
+                        <Route path="/inventory" exact component={Inventory} />
+                    </Switch>
+                </Col>
+            </Row>
+        </Container>*/}
+
+        <Footer />
+
+    </Router>
+  );
+}
+
+export default App;
